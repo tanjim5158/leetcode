32a23e9014e0da14844dcdecf8e69212f89b1143
MD Tanjim Molla <261-15-518@diu.edu.bd>
2026-09-03 09:23:16 +0600

    Update 1,two_sum.cpp

--- 1,two_sum.cpp
+++ 1,two_sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        
+    vector<int> twoSum(vector<int>& a, int tar) {
+        unordered_map<int,int>m;
         vector<int>ans;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+        for(int i=0;i<a.size();i++){
+            int first = a[i];
+            int sec = tar - first;
+            if(m.find(sec) != m.end()){
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
             }
+            m[first]=i;
         }
-        
-       return ans; 
-        
+        return ans;
     }
 };
